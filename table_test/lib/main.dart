0b54b09ee0a09629b93ca0ac35ed27d291f2bd98
yk <62372030+cyk-zzz@users.noreply.github.com>
2023-05-15 15:09:04 +0800

    Update main.dart

--- table_test/lib/main.dart
+++ table_test/lib/main.dart
@@ -7,104 +7,101 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'My App',
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyHomePage extends StatelessWidget {
+  MyHomePage({super.key});
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final tbl = MyTable();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('My App'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: tbl,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
 
-class MyTable extends StatelessWidget {
+class MyTable extends StatefulWidget {
   const MyTable({super.key});
+  @override
+  State<MyTable> createState() => MyTableState();
+}
+
+class MyTableState extends State<MyTable> {
+  final tr = <TableRow>[];
 
   @override
   Widget build(BuildContext context) {
-    return Table(
-      children: const [
-        TableRow(),
+    final textController = TextEditingController();
+
+    return Column(
+      children: [
+        Table(
+            border: TableBorder.all(),
+            defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+            children: tr),
+        Container(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: textController,
+            )),
+        ElevatedButton(
+          onPressed: () {
+            debugPrint('Add ${textController.text} into the table');
+            addRow(textController.text, tr, tr.length - 1);
+          },
+          child: const Text("Add Row"),
+        )
       ],
     );
+  }
+
+  TableRow createRow(String txt, List<TableRow> trP, int idx) {
+    return TableRow(children: <Widget>[
+      Container(padding: const EdgeInsets.all(10), child: Text(txt)),
+      Container(
+          padding: const EdgeInsets.all(10),
+          child: IconButton(
+              onPressed: () {
+                debugPrint("Remove table row, current length = $idx");
+                removeRow(idx);
+              },
+              icon: const Icon(Icons.delete_outline_outlined)))
+    ]);
+  }
+
+  void addRow(String txt, List<TableRow> trP, int idx) {
+    setState(() {
+      debugPrint("enter addRow() method, current length = ${trP.length}");
+      trP.add(createRow(txt, trP, idx));
+    });
+  }
+
+  void removeRow(int idx) {
+    setState(() {
+      debugPrint("enter removeRow() method, idx = $idx");
+      tr.removeAt(idx);
+    });
   }
 }
