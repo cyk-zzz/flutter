228095de07dab04a7407185bb1b97b30bcc8097b
yk <62372030+cyk-zzz@users.noreply.github.com>
2023-07-09 18:14:44 +0800

    Update flutter_firebase_test

--- flitter_firebase_test/lib/main.dart
+++ flitter_firebase_test/lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  FirebaseAuth.instance.authStateChanges().listen((User? user) {
+    if (user == null) {
+      print('User is currently signed out!');
+    } else {
+      print('User is signed in!');
+    }
+  });
+
   runApp(const MainApp());
 }
 
